d22af527622469070b90880b7bea02554e8882bd
patrickdeleon778 <patrickdeleon778@gmail.com>
2023-12-02 02:31:03 -0500

    Added the category buttons with the dummyData

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { StatusBar } from "expo-status-bar";
 import {
   View,
@@ -7,11 +7,32 @@ import {
   Image,
   TextInput,
   TouchableOpacity,
+  StyleSheet,
 } from "react-native";
 import customColors from "../config/customColors";
 import { FontAwesome5 } from "@expo/vector-icons";
 
 const HomeScreen = () => {
+  const [foodData, setFoodData] = useState([
+    {
+      id: 1,
+      name: "Chicken",
+      image: require("../assets/images/PQ2_Teddie.webp"),
+    },
+    {
+      id: 2,
+      name: "Seafood",
+      image: require("../assets/images/PQ2_Teddie.webp"),
+    },
+    { id: 3, name: "Beef", image: require("../assets/images/PQ2_Teddie.webp") },
+    { id: 4, name: "Pork", image: require("../assets/images/PQ2_Teddie.webp") },
+    {
+      id: 5,
+      name: "Veggie",
+      image: require("../assets/images/PQ2_Teddie.webp"),
+    },
+  ]);
+
   return (
     <View
       className="flex-1 bg-white"
@@ -70,11 +91,39 @@ const HomeScreen = () => {
             />
           </TouchableOpacity>
         </View>
-
         
+        <View style={{ marginTop: 20, flexDirection: 'row', alignItems: "center" }}>
+        {foodData.map((item) => (
+          <TouchableOpacity
+            key={item.id}
+            onPress={() => console.log(item.name + " pressed")}
+            style={styles.button}
+          >
+            <Image source={item.image} style={styles.image} />
+            <Text style={styles.text}>{item.name}</Text>
+          </TouchableOpacity>
+        ))}
+        </View>
       </ScrollView>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    margin: 5,
+  },
+  image: {
+    width: 60,
+    height: 60,
+    borderRadius: 50, 
+    backgroundColor: customColors.light,
+  },
+  text: {
+    color: "#666",
+    fontSize: 16,
+  },
+});
 
 export default HomeScreen;
